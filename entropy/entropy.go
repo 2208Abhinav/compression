2e7cd99e99f8792f2361f4aab3e83f702168b1ce
Abhinav Singh <abhinavsingh22@hotmail.com>
2019-05-03 15:27:10 +0530

    entropy: Add package to calculate entropy of given data.
    The entropy is calculated by using probability model
    and iid assumption.

---
+++ entropy/entropy.go
@@ -0,0 +1,71 @@
+package entropy
+/**************************************************************************
+	This package will calculate entropy for given data string using:
+	Probability model and iid(independent and identical distribution)
+	assumption.
+**************************************************************************/
+
+import (
+	"bufio"
+	"math"
+	"os"
+)
+
+// DataFrequencyMap will take data from input
+// and prepare a data frequency map.
+func DataFrequencyMap() map[rune]int {
+	frequencyMap := make(map[rune]int)
+
+	in := bufio.NewReader(os.Stdin)
+
+	// \n is also included in the data.
+	line, _ := in.ReadString('\n')
+
+	for _, char := range line {
+		// Initially the value is 0 for all characters and we
+		// increase it by 1 whenever we encounter the character.
+		frequencyMap[char] += 1
+	}
+
+	return frequencyMap
+}
+
+// DataProbabilityDistribution will return an array
+// that holds the probability distribution of data
+func DataProbabilityDistribution(dataFreqMap map[rune]int) []float32 {
+	var probabilityDist []float32
+	var totalChars int
+
+	for _, freq := range dataFreqMap {
+		totalChars += freq
+	}
+
+	for _, freq := range dataFreqMap {
+		probabilityDist = append(probabilityDist, float32(freq)/float32(totalChars))
+	}
+
+	return probabilityDist
+}
+
+// Entropy will calculate the entropy for given
+// data frequency map and return float32 value
+// which represents entropy as (x)bits/sample
+func Entropy(dataFreqMap map[rune]int) float32 {
+	var entropy float32
+
+	probDist := DataProbabilityDistribution(dataFreqMap)
+
+	// entropy = sigma(-P(Ai)log(P(Ai)))
+	/*
+		 base of log  |   unit of entropy
+		      2       |    bits/sample
+		      e       |    nats/sample
+		      10      |   hartleys/sample
+	 */
+
+	for _, probability := range probDist {
+		entropy -= probability * float32(math.Log2(float64(probability)))
+	}
+
+	return entropy
+}
